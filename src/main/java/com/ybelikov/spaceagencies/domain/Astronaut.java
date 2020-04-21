d65de93974f98e5203fca6acd0c570dee7b16325
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-04-21 16:41:12 +0300

    Create crew model

---
+++ src/main/java/com/ybelikov/spaceagencies/domain/Astronaut.java
@@ -0,0 +1,8 @@
+package com.ybelikov.spaceagencies.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Astronaut {
+
+}
--- src/main/java/com/ybelikov/spaceagencies/domain/Crew.java
+++ src/main/java/com/ybelikov/spaceagencies/domain/Crew.java
@@ -1,15 +1,50 @@
 package com.ybelikov.spaceagencies.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
-
 public class Crew {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private int numberOfMembers;
+    @OneToMany
+    private Set<Astronaut> astronauts;
     @OneToOne
     private Mission mission;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getNumberOfMembers() {
+        return numberOfMembers;
+    }
+
+    public void setNumberOfMembers(int numberOfMembers) {
+        this.numberOfMembers = numberOfMembers;
+    }
+
+    public Set<Astronaut> getAstronauts() {
+        return astronauts;
+    }
+
+    public void setAstronauts(Set<Astronaut> astronauts) {
+        this.astronauts = astronauts;
+    }
+
+    public Mission getMission() {
+        return mission;
+    }
+
+    public void setMission(Mission mission) {
+        this.mission = mission;
+    }
+
+
 }
