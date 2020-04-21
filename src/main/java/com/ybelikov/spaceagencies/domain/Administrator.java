2eae62ea14400c928f8dd1d1bc6b24ef629be8a8
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-04-21 17:07:00 +0300

    Create getters and setters for administrator model

--- src/main/java/com/ybelikov/spaceagencies/domain/Administrator.java
+++ src/main/java/com/ybelikov/spaceagencies/domain/Administrator.java
@@ -1,16 +1,59 @@
 package com.ybelikov.spaceagencies.domain;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-
 public class Administrator {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String surname;
-
+    private Date birthDate;
     @OneToOne
     private Agency agency;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Agency getAgency() {
+        return agency;
+    }
+
+    public void setAgency(Agency agency) {
+        this.agency = agency;
+    }
 }
