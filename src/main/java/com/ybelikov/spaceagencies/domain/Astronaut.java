82290809100958df8e3698357cc246e65538f22c
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-04-21 16:45:29 +0300

    Create astronaut model

--- src/main/java/com/ybelikov/spaceagencies/domain/Astronaut.java
+++ src/main/java/com/ybelikov/spaceagencies/domain/Astronaut.java
@@ -1,8 +1,70 @@
 package com.ybelikov.spaceagencies.domain;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Astronaut {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private  String surname;
+    
+    @ManyToOne
+    private Country country;
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    private Date birthDate;
+    private String rank;
+
+
+
 
 }
