71c3bf4aaa9fe9e6dc66f63118830bf50f050d0d
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-04-21 16:12:22 +0300

    Generate getters and setters in mission model

--- src/main/java/com/ybelikov/spaceagencies/domain/Crew.java
+++ src/main/java/com/ybelikov/spaceagencies/domain/Crew.java
@@ -1,4 +1,15 @@
 package com.ybelikov.spaceagencies.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+
 public class Crew {
+    @Id
+    private long id;
+    private int numberOfMembers;
+    @OneToOne
+    private Mission mission;
 }
--- src/main/java/com/ybelikov/spaceagencies/domain/Mission.java
+++ src/main/java/com/ybelikov/spaceagencies/domain/Mission.java
@@ -12,6 +12,7 @@ public class Mission {
     private String name;
     private Date start;
     private Date end;
+    private boolean isRobotic;
 
     @OneToOne
     private Crew crew;
@@ -19,4 +20,59 @@ public class Mission {
     @ManyToOne
     private Program program;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public boolean isRobotic() {
+        return isRobotic;
+    }
+
+    public void setRobotic(boolean robotic) {
+        isRobotic = robotic;
+    }
+
+    public Crew getCrew() {
+        return crew;
+    }
+
+    public void setCrew(Crew crew) {
+        this.crew = crew;
+    }
+
+    public Program getProgram() {
+        return program;
+    }
+
+    public void setProgram(Program program) {
+        this.program = program;
+    }
 }
