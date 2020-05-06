b5faed72fa2ee18e425b04c0fb6cb3945fbfe3da
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-05-06 13:52:05 +0300

    Create repository for crew model

---
+++ src/main/java/com/ybelikov/spaceagencies/repositories/CrewRepository.java
@@ -0,0 +1,8 @@
+package com.ybelikov.spaceagencies.repositories;
+
+import com.ybelikov.spaceagencies.domain.Crew;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CrewRepository extends CrudRepository<Crew, Long> {
+
+}
