89d8f8c7c554f36d8228c16d5b05a81263ea6dec
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-05-06 14:02:13 +0300

    Create repository for astronaut model

---
+++ src/main/java/com/ybelikov/spaceagencies/repositories/AstronautRepository.java
@@ -0,0 +1,8 @@
+package com.ybelikov.spaceagencies.repositories;
+
+import com.ybelikov.spaceagencies.domain.Astronaut;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AstronautRepository extends CrudRepository<Astronaut, Long> {
+    
+}
