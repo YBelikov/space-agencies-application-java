6b9190db4b45cc0d687f54cbae1b42ae6ac8649f
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-05-06 14:03:49 +0300

    Update AstronautRepository.java

--- src/main/java/com/ybelikov/spaceagencies/repositories/AstronautRepository.java
+++ src/main/java/com/ybelikov/spaceagencies/repositories/AstronautRepository.java
@@ -4,5 +4,5 @@ import com.ybelikov.spaceagencies.domain.Astronaut;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AstronautRepository extends CrudRepository<Astronaut, Long> {
-    
+
 }
